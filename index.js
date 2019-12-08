29f0caef17ab26d1ffa10a6d1ae3940c81f31715
Ahmed Hassan <ahmedalhassany1@gmail.com>
2019-12-08 08:04:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ class Breakfast{
 }
 
 class Lunch{
-  constructor(salad ,soup){
-    this.food =food
+  constructor(salad ,soup,drink){
+    this.salad =salad
+    this.soup =soup
     this.drink =drink
   }
 }
